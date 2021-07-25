3009d36f92f3b270596afb678af49859d36819a7
zongjie hu <kaiji@xiaohongshu.com>
2021-07-25 23:09:09 +0800

    add mermaid

--- init.js
+++ init.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-window.addEventListener("load", () =>
-  mermaid.initialize({ startOnLoad: true })
-);
-// mermaid.initialize({ startOnLoad: true })
+// window.addEventListener("load", () =>
+//   mermaid.initialize({ startOnLoad: true })
+// );
+mermaid.initialize({ startOnLoad: true })
